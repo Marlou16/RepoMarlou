7f64131d2758e781274c6d85efd155b55dd28da1
Marlou Pors <marloupors@gmail.com>
2015-03-27 13:43:05 +0100

    Clean-up

--- ET4389 - CN Assignment Code/matlab-networks-toolbox-master/aveNeighborDeg.m
+++
@@ -1,7 +0,0 @@
-function [ ret ] = aveNeighborDeg( A )
-%aveNeighborDeg Alias of averageNeighborDegree
-%   See averageNeighborDegree for info.
-ret = averageNeighborDegree(A);
-
-end
-
--- ET4389 - CN Assignment Code/matlab-networks-toolbox-master/linkDensity.m
+++
@@ -1,5 +0,0 @@
-function d=linkDensity(A)
-%LINKDENSITY Computes the edge density of a graph.
-% Aliased from edgeDensity. See edgeDensity for documentation.
-
-[ d ] = edgeDensity( A );
