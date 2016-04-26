f0543278775cc788934df4c701c3d41352bd0b03
Marlou Pors <marloupors@gmail.com>
2016-04-26 14:54:29 +0200

    Deleted stuff regarding Complex Networks course

--- ET4389 - CN Assignments All Code/matlab-networks-toolbox-master/avePathLength.m
+++
@@ -1,7 +0,0 @@
-function [ l, path_lengths ] = avePathLength( A )
-%AVEPATHLENGTH An alias of averagePathLength
-%   See averagePathLength for more information.
-[l, path_lengths]  = averagePathLength(A);
-
-end
-
